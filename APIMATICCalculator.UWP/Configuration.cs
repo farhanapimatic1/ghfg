05f77d686aa610fd2cb866b28d689f768c8e3a23
Unknown <git@apimatic.io>
2018-01-02 08:55:49 +0000

    fgh

---
+++ APIMATICCalculator.UWP/Configuration.cs
@@ -0,0 +1,11 @@
+using APIMATICCalculator.UWP.Models;
+namespace APIMATICCalculator.UWP
+{
+    public partial class Configuration
+    {
+
+        //The base Uri for API calls
+        public static string BaseUri = "http://examples.apimatic.io/apps/calculator";
+
+    }
+}
