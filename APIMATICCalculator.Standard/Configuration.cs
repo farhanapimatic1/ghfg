2de4477b9739f6482db47289b37e10b1ac851386
Unknown <git@apimatic.io>
2018-01-02 08:54:20 +0000

    ;

---
+++ APIMATICCalculator.Standard/Configuration.cs
@@ -0,0 +1,11 @@
+using APIMATICCalculator.Standard.Models;
+namespace APIMATICCalculator.Standard
+{
+    public partial class Configuration
+    {
+
+        //The base Uri for API calls
+        public static string BaseUri = "http://examples.apimatic.io/apps/calculator";
+
+    }
+}
